b43e0922d02108b6d3d46aae3e95b1a4aaa687b0
JihyeHwang09 <raye9053@gmail.com>
2018-12-19 00:09:38 +0900

    merge conflict 해결

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-const {Provider, Consumer} = React.createContext();
+import api from "../api";
 
-export default class UserProvider extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+import React, { Component } from "react";
+
+const { Provider, Consumer } = React.createContext({
+  username: "",
+  id: 0,
+  login: () => {},
+  logout: () => {}
+});
+
+export default class UserContext extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: null,
+      username: null
+    };
   }
-}
+  async componentDidMount() {
+    if (localStorage.getItem("token")) {
+      await this.refreshUser();
+    }
+  }
 
+  async login(username, password) {
+    const res = await api.post("/api-token-auth/", {
+      username,
+      password
+    });
+  }
+  localStorage.setItem('token', res.data.token);
+  await this.refreshUser();
+  // this.props.
+
+
+  logout() {
+    // 로컬 스토리지에서 토큰 제거
+    localStorage.removeItem('token');
+    // 사용자 정보 캐시 초기화
+    this.setState({
+      id: null,
+      username: null,
+    })
+  }
+
+}
+  render() {
+    return <div />;
+  }
